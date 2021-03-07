782c9bd28512cd5489940e152ac5a2a675c949de
Nabeel <nabeelfarid@gmail.com>
2021-03-07 16:55:52 +0500

    Adding bootstrap as a package for service worker app shell caching

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -3,36 +3,36 @@ import reactLogo from '../images/react.svg';
 
 export const Header = () => {
   return (
-    <div className="p-md-4 p-2 mb-4 shadow bg-primary bg-gradient d-flex justify-content-between align-items-center">
+    <div className="p-md-3 p-2 mb-4 shadow bg-primary bg-gradient d-flex justify-content-between align-items-center">
 
       <div className="text-white">
         <h1>Quiz App</h1>
       </div>
 
-      <div className='row gx-2'>
+      <div className='row gx-3 align-items-center'>
         <div className="col">
-          <a title="React JS" className="btn btn-primary" href="https://reactjs.org/" target="_blank" rel="noopener noreferrer">
-            <img src={reactLogo} alt='React JS' height={24} width={24}></img>
+          <a title="React JS" className="" href="https://reactjs.org/" target="_blank" rel="noopener noreferrer">
+            <img src={reactLogo} alt='React JS' height={25} width={25}></img>
           </a>
         </div>
         <div className="col">
-          <a title="TypeScript" className="btn btn-primary" href="https://www.typescriptlang.org/" target="_blank" rel="noopener noreferrer">
-            <img src={tsLogo} alt='TypeScript' height={24} width={24}></img>
+          <a title="TypeScript" className="" href="https://www.typescriptlang.org/" target="_blank" rel="noopener noreferrer">
+            <img src={tsLogo} alt='TypeScript' height={25} width={25}></img>
           </a>
         </div>
         <div className="col">
-          <a title="Bootstrap" className="btn btn-primary" href="https://getbootstrap.com/" target="_blank" rel="noopener noreferrer">
-            <i className="bi bi-bootstrap" style={{ fontSize: "1.2rem" }}></i>
+          <a title="Bootstrap" className="text-white" href="https://getbootstrap.com/" target="_blank" rel="noopener noreferrer">
+            <i className="bi bi-bootstrap" style={{ fontSize: "1.5rem" }}></i>
           </a>
         </div>
         <div className="col">
-          <a title="Quiz API" className="btn btn-primary" href="https://opentdb.com/" target="_blank" rel="noopener noreferrer">
-            <i className="bi bi-sliders" style={{ fontSize: "1.2rem" }}></i>
+          <a title="Quiz API" className="text-white" href="https://opentdb.com/" target="_blank" rel="noopener noreferrer">
+            <i className="bi bi-sliders" style={{ fontSize: "1.5rem" }}></i>
           </a>
         </div>
         <div className="col">
-          <a title="Github Repo" className="btn btn-primary" href="https://github.com/nabeelfarid/quiz-app" target="_blank" rel="noopener noreferrer">
-            <i className="bi bi-github" style={{ fontSize: "1.2rem" }}></i>
+          <a title="Github Repo" className="text-white" href="https://github.com/nabeelfarid/quiz-app" target="_blank" rel="noopener noreferrer">
+            <i className="bi bi-github" style={{ fontSize: "1.5rem" }}></i>
           </a>
         </div>
       </div>
